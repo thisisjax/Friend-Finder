82ff20970b65cd189ae82c72a3d64b4b06adc54f
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-08-29 09:34:13 -0400

    push friends app

--- data/friends.js
+++ data/friends.js
@@ -1,17 +1,38 @@
-var friends = [];
+// // Friends (DATA)
+// // =============================================================
+var friends = [
+    {
+        name: "Gio the Guido",
+        photo: "https://pbs.twimg.com/profile_images/2625180694/image_400x400.jpg",
+        score: "1, 1, 1, 1, 1, 5, 1, 1, 1, 1",
+    },
+    {
+        name: "Leaf",
+        photo: "https://www.menshairstylesnow.com/wp-content/uploads/2017/01/How-To-Style-A-Man-Bun.jpg",
+        score: "3, 3, 3, 3, 3, 1, 3, 3, 5, 3",
+    },
+    {
+        name: "Chad",
+        photo: "https://i1.wp.com/secureservercdn.net/427.678.godaddywp.com/wp-content/uploads/d11f71e428644d3734adcaeda004fee0-preppy-boys-southern-gentleman.jpg?w=736&ssl=1",
+        score: "5, 5, 5, 5, 5, 1, 5, 5, 5, 5",
+    },
+    {
+        name: "Kimburlie",
+        photo: "https://pbs.twimg.com/media/CuLUsw-XYAApYbQ.jpg",
+        score: "5, 5, 5, 5, 5, 1, 5, 5, 5, 5",
+    },
+    {
+        name: "Summer",
+        photo: "http://everythreeweekly.com/wp-content/uploads/2017/02/b8fa6fdda8f243d5c8b5f6ed92dabb2a.jpg",
+        score: "3, 3, 3, 3, 3, 1, 3, 3, 5, 3",
+    },
+    {
+        name: "Vicky",
+        photo: "https://2ctptqj9vf3lafyt2rkh1qto-wpengine.netdna-ssl.com/wp-content/uploads/2017/12/woahvicky.jpg",
+        score: "1, 1, 1, 1, 1, 5, 1, 1, 1, 1",
+    },
+];
 
-$("#submit-survey").on("click", function(event) {
-    event.preventDefault();
-    var newFriends = {
-      name: $("#name").val().trim(),
-      photo: $("#photo").val().trim(),
-     
-    };
+module.exports = friends;
 
-    // // Question: What does this code do??
-    // $.post("/api/friends", newFriends)
-    //   .then(function(data) {
-    //     console.log("friends.js", data);
-        
-    //   });
-  });
+
--- server.js
+++ server.js
@@ -2,28 +2,30 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+
+var app = express();
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//Route Files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/home.html"));
-  });
+// // Basic route that sends the user first to the AJAX Page
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "public/home.html"));
+//   });
   
   
-  app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/survey.html"));
-  });
+//   app.get("/survey", function (req, res) {
+//     res.sendFile(path.join(__dirname, "public/survey.html"));
+//   });
 
 
 // Starts the server to begin listening
